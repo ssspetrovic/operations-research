a2c3f1c11ec09d4bfafb59f444e2cdac95fc734d
Srdjan Petrovic <s.petrovic852@gmail.com>
2024-01-08 00:39:21 +0100

    Add types to transport

--- transport.py
+++ transport.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class Transport:
-    def __init__(self, M, S, D):
+    def __init__(self, M: np.ndarray, S: np.ndarray, D: np.ndarray):
         # Check if the dimensions and supplies/demands are balanced
         assert M.shape[0] == S.shape[0], "Supply size must be equal to the size of rows."
         assert M.shape[1] == D.shape[0], "Demand size must be equal to the size of columns."
@@ -16,7 +16,7 @@ class Transport:
         self.col_size = M.shape[1]
         self.A = np.zeros((self.row_size, self.col_size))
 
-    def copy_data(self):
+    def copy_data(self) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         # Create copies of the original data matrices
         M = self.M.copy()
         S = self.S.copy()
@@ -25,23 +25,23 @@ class Transport:
                           )  # Reset the solution matrix
         return M, S, D
 
-    def calculate_z(self):
+    def calculate_z(self) -> float:
         # Calculate and return the total cost Z
         return np.sum(self.A * M)
 
-    def print_solution(self):
+    def print_solution(self) -> None:
         # Print the final solution matrix and the total cost Z
         print("\nFinal matrix: ")
         print(self.A)
         print("\nZ =", self.calculate_z())
         print()
 
-    def get_min_index(self, M) -> tuple[int, int]:
+    def get_min_index(self, M: np.ndarray) -> tuple[int, int]:
         # Get the index of the minimum value in the given matrix
         min_index = np.argmin(M)
         return np.unravel_index(min_index, M.shape)
 
-    def solve_north_west(self):
+    def solve_north_west(self) -> None:
         # North-West Corner Rule method for initial solution
         M, S, D = self.copy_data()
 
@@ -57,7 +57,7 @@ class Transport:
                 S[i] -= self.A[i, j]
                 D[j] -= self.A[i, j]
 
-    def solve_lowest_cost(self):
+    def solve_lowest_cost(self) -> None:
         # Lowest Cost method for initial solution
         M, S, D = self.copy_data()
 
@@ -82,7 +82,7 @@ class Transport:
             unprocessed_rows = [r for r in unprocessed_rows if S[r] != 0]
             unprocessed_cols = [c for c in unprocessed_cols if D[c] != 0]
 
-    def solve_vogel(self):
+    def solve_vogel(self) -> None:
         # Vogel's Approximation method for initial solution
         M, S, D = self.copy_data()
 
